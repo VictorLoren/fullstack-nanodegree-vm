07573af69da88c884ddcfdb272ead680655c51ba
Victor Loren <VictorLoren@users.noreply.github.com>
2015-11-03 12:19:03 -0800

    Update forumdb.py to add/read to/from 'posts' table

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,11 +1,11 @@
 #
 # Database access functions for the web forum.
-# 
+#
 
 import time
+import psycopg2
 
-## Database connection
-DB = []
+
 
 ## Get posts from database.
 def GetAllPosts():
@@ -16,8 +16,16 @@ def GetAllPosts():
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
+    # Database connection and fetch all results
+    DB = psycopg2.connect("dbname=forum")
+    cursor = DB.cursor()
+    cursor.execute("SELECT * FROM posts ORDER BY time DESC")
+    results = cursor.fetchall()
+    # Get a proper list to be returned
+    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in results]
     posts.sort(key=lambda row: row['time'], reverse=True)
+    # Close connection and return list
+    DB.close()
     return posts
 
 ## Add a post to the database.
@@ -28,4 +36,10 @@ def AddPost(content):
       content: The text content of the new post.
     '''
     t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    # Database connection and insert
+    DB = psycopg2.connect("dbname=forum")
+    cursor = DB.cursor()
+    cursor.execute("INSERT INTO posts (content,time) VALUES ('%s','%s')"
+        %(content,t))
+    DB.commit()
+    DB.close()
